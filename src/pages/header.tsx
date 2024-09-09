dc5c58110b2b0d74688ccefc64f3b54d2f7ffe34
EstebanC-302 <estebanrcc69@gmail.com>
2024-09-09 19:36:54 -0300

    mi primer commit

---
+++ src/pages/header.tsx
@@ -0,0 +1,25 @@
+export function Header() {
+  return(
+    <header className="bg-white">
+  <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    {/* <div className="flex lg:flex-1">
+      <a href="#" className="-m-1.5 p-1.5">
+        <span className="sr-only">Your Company</span>
+      </a>
+    </div> */}
+    {/* <div className="flex lg:hidden">
+      <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+        <span className="sr-only">Open main menu</span>
+        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+    </div> */}
+    <div className="hidden lg:flex lg:gap-x-12">
+      <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Moni-Bot</a>
+      <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Login</a>
+    </div>
+  </nav>
+</header>
+)
+}
